dedf4e145411334b2ecdf97f0eb1a9ced0667a71
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2026-08-13 04:50:22 +0300

    HOME HERO SECTION DESIGN

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import FeaturesSection from "@/components/home/FeaturesSection";
+import HeroSection from "@/components/home/HeroSection";
 import PropertyOwnersSection from "@/components/home/PropertyOwnersSection";
 
 export default function Home() {
   return (
     <>
+      <HeroSection />
       <PropertyOwnersSection />
       <FeaturesSection />
     </>
---
+++ components/home/HeroSection.tsx
@@ -0,0 +1,76 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function HeroSection() {
+  return (
+    <section className="min-h-screen py-20 flex flex-col justify-center relative">
+      <span>
+        <Image
+          src="/home-hero-image.jpg"
+          alt="Hero Image"
+          width={4096}
+          height={2736}
+          className="absolute inset-0 w-full h-full object-cover -z-10"
+        />
+      </span>
+      <span className="absolute left-0 top-0 w-full h-full bg-black/70 -z-10"></span>
+
+      <div className="container">
+        <h1 className="text-3xl md:text-6xl font-extrabold text-[#F8F8F8] text-center md:text-right leading-[50px] md:leading-[80px]">
+          ذا كابيتــــــال سويتــــــس <br /> للضيافــــة والفندقـــة
+        </h1>
+        <p className="text-xl text-center md:text-right md:text-4xl font-extralight text-[#E7DECA] mt-4">
+          ادارة تــرفــع القــيــمــــة... وضـــيـــافة تــصــنـــع
+          التـــجــربــة
+        </p>
+
+        <div className="mt-8 md:flex items-center gap-4 max-md:space-y-4">
+          <Link
+            href="#"
+            className="bg-white w-full text-black rounded-md px-14 py-3 flex items-center justify-center gap-4 md:w-fit hover:opacity-80 duration-75"
+          >
+            <span>احجز الآن</span>
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6 "
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+                />
+              </svg>
+            </span>
+          </Link>
+          <Link
+            href="#"
+            className="bg-transparent w-full text-white border border-white rounded-md px-10 py-3 flex items-center justify-center gap-4 md:w-fit hover:opacity-80 duration-75"
+          >
+            <span>تواصل معنا</span>
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6 "
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+                />
+              </svg>
+            </span>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+}
