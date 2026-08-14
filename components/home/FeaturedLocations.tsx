a995f8a6f09d9b18c1b326295d816199739317ce
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2026-08-14 22:41:00 +0300

    FeaturedLocations + WP

--- components/home/FeaturedLocations.tsx
+++ components/home/FeaturedLocations.tsx
@@ -1,6 +1,30 @@
 import Image from "next/image";
 
-export default function FeaturedLocations() {
+export default async function FeaturedLocations() {
+  const featuredLocationsListRes = await fetch(
+    `${process.env.NEXT_PUBLIC_WORDPRESS_API_URL}/featured-locations?_fields=id,acf&acf_format=standard`,
+  );
+
+  const featuredLocationsList: {
+    id: number;
+    acf: {
+      title: string;
+      images: {
+        image_1: string | false;
+        image_2: string | false;
+        image_3: string | false;
+        image_4: string | false;
+        image_5: string | false;
+        image_6: string | false;
+        image_7: string | false;
+        image_8: string | false;
+        image_9: string | false;
+        image_10: string | false;
+      };
+    };
+  }[] = await featuredLocationsListRes.json();
+  console.log("Featured Locations List:", featuredLocationsList);
+
   return (
     <section className="font-thamnyah container py-20">
       <h2 className="font-black text-5xl leading-[60px] text-center bg-gradient-to-l from-white to-80% to-[#bdbdbd] bg-clip-text text-transparent my-10">
@@ -8,51 +32,34 @@ export default function FeaturedLocations() {
       </h2>
 
       <div className="bg-[#3d29003b] w-fit mx-auto border border-[#3D2900] p-3 md:p-2 rounded-xl md:rounded-full flex flex-wrap justify-center items-center gap-2">
-        <button className="bg-[#C8AD6A] cursor-pointer hover:opacity-80 border border-[#3D2900] text-[#0B0800] font-medium text-lg px-6 py-2 rounded-full">
-          حي العـــــارض
-        </button>
-        <button className="border min-w-30  cursor-pointer hover:opacity-80 border-[#3D2900] text-[#E7DECA] font-medium text-lg px-6 py-2 rounded-full">
-          حطين
-        </button>
-        <button className="border min-w-30  cursor-pointer hover:opacity-80 border-[#3D2900] text-[#E7DECA] font-medium text-lg px-6 py-2 rounded-full">
-          القيـــروان
-        </button>
-        <button className="border min-w-30  cursor-pointer hover:opacity-80 border-[#3D2900] text-[#E7DECA] font-medium text-lg px-6 py-2 rounded-full">
-          الملز
-        </button>
-        <button className="border min-w-30  cursor-pointer hover:opacity-80 border-[#3D2900] text-[#E7DECA] font-medium text-lg px-6 py-2 rounded-full">
-          الرمال
-        </button>
+        {featuredLocationsList.map((location) => (
+          <button
+            key={location.id}
+            className="bg-[#C8AD6A] cursor-pointer hover:opacity-80 border border-[#3D2900] text-[#0B0800] font-medium text-lg px-6 py-2 rounded-full"
+          >
+            {location.acf.title}
+          </button>
+        ))}
       </div>
 
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10">
-        <div className="h-60 rounded-xl overflow-hidden relative">
-          <Image
-            src="/home-hero-image.jpg"
-            alt="Featured Locations"
-            width={1200}
-            height={800}
-            className="w-full h-full object-cover"
-          />
-        </div>
-        <div className="h-60 rounded-xl overflow-hidden relative">
-          <Image
-            src="/home-hero-image.jpg"
-            alt="Featured Locations"
-            width={1200}
-            height={800}
-            className="w-full h-full object-cover"
-          />
-        </div>
-        <div className="h-60 rounded-xl overflow-hidden relative">
-          <Image
-            src="/home-hero-image.jpg"
-            alt="Featured Locations"
-            width={1200}
-            height={800}
-            className="w-full h-full object-cover"
-          />
-        </div>
+        {featuredLocationsList.map((location) => {
+          const imagesList = Object.values(location.acf.images).filter(
+            (image) => image !== false,
+          );
+
+          return imagesList.map((image, index) => (
+            <div className="h-60 rounded-xl overflow-hidden relative">
+              <Image
+                src={image || "/home-hero-image.jpg"}
+                alt="Featured Locations"
+                width={1200}
+                height={800}
+                className="w-full h-full object-cover"
+              />
+            </div>
+          ));
+        })}
       </div>
 
       <div className="flex justify-center gap-2 mt-10">
