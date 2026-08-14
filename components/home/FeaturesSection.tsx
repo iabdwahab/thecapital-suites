1bf73cfb476c8e315b659770107728f67b9a3273
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2026-08-14 04:13:14 +0300

    FeaturesSection + WP

--- components/home/FeaturesSection.tsx
+++ components/home/FeaturesSection.tsx
@@ -1,6 +1,18 @@
 import Image from "next/image";
 
-export default function FeaturesSection() {
+export default async function FeaturesSection() {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_WORDPRESS_API_URL}/dealing-features?_fields=id,acf&acf_format=standard`,
+  );
+  const featuresList: {
+    id: number;
+    acf: {
+      icon: string;
+      title: string;
+      description: string;
+    };
+  }[] = await res.json();
+
   return (
     <section className="py-10 container">
       <div>
@@ -20,34 +32,40 @@ export default function FeaturesSection() {
       </div>
 
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10">
-        <FeatureCard />
-        <FeatureCard />
-        <FeatureCard />
-        <FeatureCard />
-        <FeatureCard />
-        <FeatureCard />
+        {featuresList.map((feature) => (
+          <FeatureCard
+            key={feature.id}
+            icon={feature.acf.icon}
+            title={feature.acf.title}
+            description={feature.acf.description}
+          />
+        ))}
       </div>
     </section>
   );
 }
 
-function FeatureCard() {
+function FeatureCard({
+  icon,
+  title,
+  description,
+}: {
+  icon: string;
+  title: string;
+  description: string;
+}) {
   return (
     <div className="relative group overflow-hidden rounded-xl p-8 border border-[#C9A45526] flex flex-col items-center text-center">
       <Image
-        src="/feature-placeholder.svg"
-        alt="Feature 1"
+        src={icon || "/feature-placeholder.svg"}
+        alt={title}
         width={36}
         height={36}
         className=""
       />
 
-      <h3 className="font-semibold text-lg text-[#F0E0C0] mt-4">
-        خبرة واسعة بالسوق
-      </h3>
-      <p className="text-[#7D6E58] font-light mt-1">
-        معرفة عميقة بالسوق السعودي والخليجي
-      </p>
+      <h3 className="font-semibold text-lg text-[#F0E0C0] mt-4">{title}</h3>
+      <p className="text-[#7D6E58] font-light mt-1">{description}</p>
 
       <span className="absolute bottom-0 right-0 transform translate-x-1/2 translate-y-1/2 w-[100px] h-[100px] blur-[100px] group-hover:bg-[#ffa600] rounded-full transition duration-300"></span>
       <span className="absolute bottom-0 left-0 transform -translate-x-1/2 translate-y-1/2 w-[100px] h-[100px] blur-[100px] group-hover:bg-[#ffa600] rounded-full transition duration-300"></span>
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "thecapital-suites.local",
+        port: "",
+        pathname: "/wp-content/**",
+      },
+    ],
+    dangerouslyAllowLocalIP: true,
+  },
 };
 
 export default nextConfig;
