cc4c95d703872d16d5ed97ec453fb825e5ed1a52
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2026-08-14 04:27:23 +0300

    PlatformsSection + WP

--- components/home/PlatformsSection.tsx
+++ components/home/PlatformsSection.tsx
@@ -1,4 +1,24 @@
-export default function PlatformsSection() {
+export default async function PlatformsSection() {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_WORDPRESS_API_URL}/booking-platforms?_fields=id,acf&acf_format=standard`,
+  );
+  const platformsList: {
+    id: number;
+    acf: {
+      title: string;
+      platforms_list: {
+        platform_1: string;
+        platform_2: string;
+        platform_3: string;
+        platform_4: string;
+        platform_5: string;
+        platform_6: string;
+        platform_7: string;
+        platform_8: string;
+      };
+    };
+  }[] = await res.json();
+
   return (
     <section className="py-20 container">
       <h2 className="font-bold text-3xl md:text-4xl text-[#E0BC78] text-center mt-4 leading-[46px]">
@@ -9,54 +29,18 @@ export default function PlatformsSection() {
       </p>
 
       <div className="mt-10 border text-center border-[#c9a45541] rounded-2xl grid md:grid-cols-2 lg:grid-cols-4">
-        <div className="p-8 md:p-10  space-y-2 border-l border-[#c9a45541]">
-          <h4 className="text-[#C9A455] font-bold">المنصات العالمية</h4>
-          <hr className="border-[#c9a45541]" />
-          <ul className="text-[#C0B09A] space-y-2">
-            <li>Booking.com</li>
-            <li>Expedia</li>
-            <li>Hotels.com</li>
-            <li>Agoda</li>
-            <li>Airbnb</li>
-            <li>Trip.com</li>
-          </ul>
-        </div>
-        <div className="p-10 space-y-2 border-l border-[#c9a45541]">
-          <h4 className="text-[#C9A455] font-bold">المنصات العالمية</h4>
-          <hr className="border-[#c9a45541]" />
-          <ul className="text-[#C0B09A] space-y-2">
-            <li>Booking.com</li>
-            <li>Expedia</li>
-            <li>Hotels.com</li>
-            <li>Agoda</li>
-            <li>Airbnb</li>
-            <li>Trip.com</li>
-          </ul>
-        </div>
-        <div className="p-10 space-y-2 border-l border-[#c9a45541]">
-          <h4 className="text-[#C9A455] font-bold">المنصات العالمية</h4>
-          <hr className="border-[#c9a45541]" />
-          <ul className="text-[#C0B09A] space-y-2">
-            <li>Booking.com</li>
-            <li>Expedia</li>
-            <li>Hotels.com</li>
-            <li>Agoda</li>
-            <li>Airbnb</li>
-            <li>Trip.com</li>
-          </ul>
-        </div>
-        <div className="p-10 space-y-2">
-          <h4 className="text-[#C9A455] font-bold">المنصات العالمية</h4>
-          <hr className="border-[#c9a45541]" />
-          <ul className="text-[#C0B09A] space-y-2">
-            <li>Booking.com</li>
-            <li>Expedia</li>
-            <li>Hotels.com</li>
-            <li>Agoda</li>
-            <li>Airbnb</li>
-            <li>Trip.com</li>
-          </ul>
-        </div>
+        {platformsList.map((platform) => (
+          <div className="p-8 md:p-10  space-y-2 border-l border-[#c9a45541]">
+            <h4 className="text-[#C9A455] font-bold">{platform.acf.title}</h4>
+            <hr className="border-[#c9a45541]" />
+            <ul className="text-[#C0B09A] space-y-2">
+              {Object.values(platform.acf.platforms_list).map(
+                (platformName, index) =>
+                  platformName && <li key={index}>{platformName}</li>,
+              )}
+            </ul>
+          </div>
+        ))}
       </div>
     </section>
   );
