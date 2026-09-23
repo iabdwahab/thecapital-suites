cdf5bd436a3573a5643654f83f699c161eeeda92
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2026-09-23 06:55:39 +0300

    Update LocationMap.tsx

--- components/LocationMap.tsx
+++ components/LocationMap.tsx
@@ -12,7 +12,7 @@ const goldIcon = new L.DivIcon({
   html: `<div style="
     width: 20px;
     height: 20px;
-    background: #836e31;
+    background: #BFA045;
     border: 2px solid #0a0a0a;
     border-radius: 50% 50% 50% 0;
     transform: rotate(-45deg);
@@ -41,7 +41,7 @@ export default function LocationMap({
         center={center}
         zoom={zoom}
         scrollWheelZoom={true}
-        className="dark-map w-full h-full"
+        className="dark-mapx w-full h-full"
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
